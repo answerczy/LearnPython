c62b1101516c550761c333870f53bced7447a2ad
Cason <answerczy@qq.com>
2019-05-28 00:04:46 +0800

    firstday

---
+++ day0527/datatype.py
@@ -0,0 +1,12 @@
+#_*_coding:utf-8_*_
+#@author：Casonchen
+result = 5 + 3
+print(type(result))  #int
+print(type(3.4))     #float
+print(type('tome')) #str
+l  = [3,7,9,4,2]
+x = l.sort() #sort函数只影响本身列表
+print(x is None)   #空对象
+print(5 > 3) #True
+print(5 < 3) #False
+print(type(5<3))  #bool类型
---
+++ day0527/variable.py
@@ -0,0 +1,25 @@
+"""
+1.== 判断字面值是否相等
+2.is 判断引用地址是否相同
+3.字符0-255的会被缓存起来，1个比特位，8个字节，2^^8
+"""
+x = 'tom'
+y = 'jerry'
+print(id(x))
+print(id(y))
+print('x与y是否相等结果:',x==y)  #结果为false
+a = 50
+b = 50
+c = 5000
+d = 5000
+print('a与b字面值相比较结果：',a == b)
+print('a与b内存地址相比较结果：',a is b) #结果为True
+print('c与d字面值相比较结果：',c == d)
+print(id(c))
+print(id(d))
+print('c与d内存地址相比较结果：',c is d) #这里的内存地址还是一样
+
+url = 'http://www.codeclassroom.com'
+link = 'http://www.codeclassroom.com'
+print(id(url))
+print(id(link))
